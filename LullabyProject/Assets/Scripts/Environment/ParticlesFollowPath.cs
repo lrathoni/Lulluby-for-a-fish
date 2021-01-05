c6b52eed6fb3f1427acf67bbab9a6bced242124e
Monica <monicalisacek@hotmail.com>
2021-01-05 15:45:57 +0100

    [Improvement]: particles follow the river's path

---
+++ LullabyProject/Assets/Scripts/Environment/ParticlesFollowPath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesFollowPath : MonoBehaviour
+{
+    public float time;
+    public float particleSystemIndex;
+    public string pathName;
+
+    void Start()
+    {
+        StartCoroutine(WaitCoroutine());
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        // time offset so that particles don't all leave at the same time
+        yield return new WaitForSeconds(particleSystemIndex/2);
+        // move particles along path (RiverPath0.... RiverPathN)
+        iTween.MoveTo( gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.linear, "time", time, "loopType", "loop") );
+    }
+}
