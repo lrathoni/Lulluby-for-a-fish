618400508f5dc489945995aee844564e7cee2529
Monica <monicalisacek@hotmail.com>
2021-01-07 18:12:06 +0100

    [Trial]: shader graph for the river

---
+++ LullabyProject/Assets/Scripts/Environment/RiverMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverMovement : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    private float angle;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        angle = 0;
+    }
+
+    void Update()
+    {
+        for (var i = 0; i < vertices.Length; i = i + 10)
+        {
+            vertices[i].z = /*Time.deltaTime*/ 0.001f * Mathf.Cos(angle);
+        }
+        Debug.Log(vertices[0].z);
+        angle++;
+
+        // assign the local vertices array into the vertices array of the Mesh.
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+}
