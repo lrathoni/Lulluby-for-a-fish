520fe5c2708bf12bd3905db39270a953cbeeec9c
Unknown <rvallee@etud.u-pem.fr>
2021-01-15 18:22:07 +0100

    Auto stash before merge of "3Denvironment" and "origin/3Denvironment"

---
+++ LullabyProject/Assets/Scripts/IO/Behaviour/DoorMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isOpen;
+    Vector3 tempPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = transform.position;
+        tempPos.y += 0.1f;
+        transform.position = tempPos;
+    }
+
+    // sqrt((51.63 - 48.8567)²)
+    
+}
