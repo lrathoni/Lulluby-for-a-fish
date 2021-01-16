6aa766e723cb2af5a6107d996e438ff54095e7d0
Unknown <rvallee@etud.u-pem.fr>
2021-01-16 20:21:47 +0100

    [Add] Add animations on doors

---
+++ LullabyProject/Assets/Scripts/Animation/DoorController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] doors;
+
+
+   
+    // Update is called once per frame
+
+    void Update()
+    {
+                
+        if(Input.GetKeyDown("space"))
+        {
+            Debug.Log("coucou");
+            
+            foreach(GameObject door in doors)
+            {
+                bool state = door.GetComponent<DoorMovement>().isOpen;
+                Debug.Log(door);
+                Debug.Log(state);
+                door.GetComponent<DoorMovement>().StartAnimation();
+                /*
+                if(state)
+                {
+                    door.GetComponent<DoorMovement>().DoorClosed();
+                }
+                
+                else
+                {
+                    door.GetComponent<DoorMovement>().DoorOpen();
+                }
+                */
+            }
+            
+        
+        }
+        
+        
+    }
+}
---
+++ LullabyProject/Assets/Scripts/Animation/DoorMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isOpen;
+    float speed = 1f;
+    bool moving = false;
+    //Vector3 tempPos;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(moving)
+        {
+            if(isOpen)
+            {
+                DoorClosed();
+            }
+            else
+            {
+                DoorOpen();
+            }
+            
+        }
+        //tempPos = transform.position;
+        //tempPos.y += 0.1f;
+        //transform.position = tempPos;
+    }
+
+   public void StartAnimation()
+   {
+       Debug.Log("début animation");
+       moving = true;
+   }
+
+   public void StopAnimation()
+   {
+       Debug.Log("fin animation");
+       moving = false;
+   }
+
+    public void DoorOpen()
+    {
+        if(transform.position.y < 53)
+        {
+            transform.position += new Vector3(0,Time.deltaTime*speed,0);
+            Debug.Log("porte s'ouvre");
+        }
+        else
+        {
+            isOpen = true;
+            StopAnimation();
+            Debug.Log("porte est ouverte");
+        }
+        //isOpen = true;
+    }
+
+    public void DoorClosed()
+    {
+        if(transform.position.y > 49)
+        {
+            transform.position -= new Vector3(0,Time.deltaTime*speed,0);
+            Debug.Log("porte se ferme");
+        }
+        else
+        {
+            isOpen = false;
+            StopAnimation();
+            Debug.Log("porte est fermée");
+        }
+    }
+
+  
+
+    // sqrt((51.63 - 48.8567)²)
+    // Open : y = 53
+    // Closed : y = 49 
+}
